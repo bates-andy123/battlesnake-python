0f08f0f41022004f96912b87d9e39b01bcc0fc59
Evan Moore <evan.moore@alumni.ubc.ca>
2016-02-20 17:19:50 -0800

    adding food40 and gold

--- app/gold.py
+++ app/gold.py
@@ -1,22 +1,22 @@
-class food40(object):
+class gold(object):
   head = 0
   data = 0
-  #coords of food positions
-  foodPos = 0
-  #number of moves to get to each food item
+  #coords of gold positions
+  goldPos = 0
+  #number of moves to get to each gold item
   moves = 0
   #position of each enemy snake
   enSnakes = 0
-  #number of moves for each enemy snake to each food item
+  #number of moves for each enemy snake to each gold item
   enSnakesMoves = 0
-  #an array defining if own snake is the closest of all snakes to each food item
+  #an array defining if own snake is the closest of all snakes to each gold item
   closest = 0
 
   def _init_(self, data, head, gameBoard):
     move = 0
     self.data = data
     self.head = self.getHead()
-    self.foodPos = self.getFood()
+    self.goldPos = self.getGold()
     self.moves = self.getMoves()
     self.enSnakes = self.getEnemySnakes()
     self.enSnakeMoves = self.getEnSnakeMoves()
@@ -30,19 +30,19 @@ class food40(object):
     for i in snakes:
       if snakes[i]["id"]=="2daa46ee-4880-4285-8572-eeaf52dba551":
         self.head=snakes[i]["coords"][0]
-  def getFood(self):
+  def getgold(self):
     board=self.gameBoard
     item = 0
     for i in board:
       for j in board[i]:
-        if board[i][j] == "F":
-          foodPos[item][0] = i
-          foodPos[item][1] = j
+        if board[i][j] == "G":
+          goldPos[item][0] = i
+          goldPos[item][1] = j
           item += 1
-  #moves for own snake to get to each food item
+  #moves for own snake to get to each gold item
   def getMoves(self):
-    for i in foodPos:
-      moves[i]=abs(head[0]-foodPos[i][0])+abs(head[1]-foodPos[i][1])
+    for i in goldPos:
+      moves[i]=abs(head[0]-goldPos[i][0])+abs(head[1]-goldPos[i][1])
   #position of each other snake
   def getEnemySnakes(self):
     board = self.gameBoard
@@ -53,16 +53,16 @@ class food40(object):
           enSnakes[item][0] = i
           enSnakes[item][1] = j
           item += 1
-  # distance of each other snake to each food item
+  # distance of each other snake to each gold item
   def getEnSnakeMoves(self):
-    for i in foodPos:
+    for i in goldPos:
       for j in enSnakes:
-        enSnakeMoves[i][j]=abs(enSnakes[j][0] - foodPos[i][0])+abs(enSnakes[j][1] - foodPos[i][1])
-  # finds if own snake is the closest to each food item
+        enSnakeMoves[i][j]=abs(enSnakes[j][0] - goldPos[i][0])+abs(enSnakes[j][1] - goldPos[i][1])
+  # finds if own snake is the closest to each gold item
   def evalClosest(self):
-  	#this will be the **INDEX** of the smallest move, used to find the food item to go to
+  	#this will be the **INDEX** of the smallest move, used to find the gold item to go to
     smallestMove = 1000
-    for i in foodPos:
+    for i in goldPos:
       for j in enSnakeMoves[i][j]:
         if moves[i] < enSnakesMoves[i][j]:
           closest[i] = y
@@ -78,20 +78,20 @@ class food40(object):
       return smallestMove
 
   def moveTo(self, item):
-    food_x = foodPos[item][0]
-    food_y = foodPos[item][1]
+    gold_x = goldPos[item][0]
+    gold_y = goldPos[item][1]
     board = self.gameBoard
     if board[head[0]+1][head[1]] != "W" or "S":
-      if (head[0]+1) <= food_x:
+      if (head[0]+1) <= gold_x:
         return "east"
     if board[head[0]-1][head[1]] != "W" or "S":
-      if (head[0]-1) >= food_x:
+      if (head[0]-1) >= gold_x:
         return "west"
     if board[head[0]][head[1]+1] != "W" or "S":
-      if (head[1]+1) <= food_y:
+      if (head[1]+1) <= gold_y:
         return "south"
     if board[head[0]][head[1]-1] != "W" or "S":
-      if (head[1]-1) >= food_y:
+      if (head[1]-1) >= gold_y:
         return "north"
     else:
       return "north"
