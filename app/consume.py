c59c54b774e1d50c9f05f13a5b94a7d34bb6ea88
Andy Bates <bates.andy123@gmail.com>
2016-02-20 16:45:55 -0800

    fix head

--- app/consume.py
+++ app/consume.py
@@ -11,33 +11,33 @@ def food70( gameData):
     y_min = 0
   else:
     y_min = y_head - foodScanMax
-  if(x_head + foodScanMax > gameData.width):
-    x_max = gameData.width
+  if(x_head + foodScanMax > gameData["width"]):
+    x_max = gameData["width"]
   else:
     x_max = x_head + foodScanMax
-  if(y_head + foodScanMax > gameData.height):
+  if(y_head + foodScanMax > gameData["height"]):
     y_max = 0
   else:
     y_max = y_head + foodScanMax
   for i in range (x_min, x_max):
     for j in range(y_min, y_max):
-      if gameData.gameBoard[i][j] == "F":
+      if gameData["gameBoard"][i][j] == "F":
           variance = fabs(x_head - i) + fabs(y_head - j)
           if (variance <= foodScanMax):
             if(fabs(x_head - i) >= fabs(y_head - j)):
               if (x_head - i < 0):
-                if gameData.gameBoard[x_head-1][y_head] == "E":
+                if gameData.['gameBoard'][x_head-1][y_head] == "E":
                   return "west"
               else:
-                if gameData.gameBoard[x_head+1][y_head] == "E":
+                if gameData.['gameBoard'][x_head+1][y_head] == "E":
                   return "east"
 
             if(fabs(x_head - i) <= fabs(y_head - j)):
               if (x_head - j < 0):
-                if gameData.gameBoard[x_head][y_head-1] == "E":
+                if gameData['gameBoard'][x_head][y_head-1] == "E":
                   return "north"
               else:
-                if gameData.gameBoard[x_head][y_head+1] == "E":
+                if gameData['gameBoard'][x_head][y_head+1] == "E":
                   return "south"
   return
 
